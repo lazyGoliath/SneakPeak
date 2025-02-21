ffffbb51f5c579fd049e8f6b240fa7fd05fc0375
lazyGoliath <lazygolith@gmail.com>
2025-02-21 14:36:58 +0530

    Completed Navbar and added swiper slider to homepage

---
+++ frontend/src/Pages/Home/HomeSlider.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+// Import Navigation styles
+import 'swiper/css/navigation';
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import './styles.css';
+
+// import required modules
+import { Navigation } from 'swiper/modules';
+import { Pagination } from 'swiper/modules';
+import { Autoplay } from 'swiper/modules';
+
+const HomeSlider = () => {
+    return (
+        <div className='homeSlider'>
+            <div className='container rounded-[20px] overflow-hidden'>
+                <Swiper navigation={true} spaceBetween={10} slidesPerView={1} loop={true}
+                pagination={{
+                clickable: true,
+                }}
+                centeredSlides={true}
+                autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: false,
+                }}
+                modules={[Autoplay, Navigation, Pagination]} className="mySwiper">
+                    <SwiperSlide>
+                        <img src="https://images-cs.stockx.com/v3/assets/blt818b0c67cf450811/bltade94cf09eea34b2/67b7ebc75dcb773a66dcf97d/EN-Nike_Foamposite_Galaxy_(New_Release)Primary_Desktop-Small.jpg?auto=webp&format=pjpg&quality=75&dpr=2&width=1246"
+                            alt="slider-1" className='w-full h-full object-cover rounded-lg'/>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src="https://images-cs.stockx.com/v3/assets/blt818b0c67cf450811/blt6a62c9cad734774a/67b799d7ed968eed9e632416/Supreme_SS25_Week_1Primary_Desktop-Small.jpg?auto=webp&format=pjpg&quality=75&dpr=2&width=1246"
+                            alt="slider-1" className='w-full h-full object-cover rounded-lg'/>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src="https://images-cs.stockx.com/v3/assets/blt818b0c67cf450811/blt7ff9d787d2789a8a/67b603ee9bea6d8f2249139c/LV_x_Murakami_Trending_-_EN_-_Primary_Desktop-Small.jpg?auto=webp&format=pjpg&quality=75&dpr=2&width=1246"
+                            alt="slider-1" className='w-full h-full object-cover rounded-lg'/>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src="https://images-cs.stockx.com/v3/assets/blt818b0c67cf450811/blt00710541b41410ca/67b60442fd5f932a7bfcd35c/EN-Leopard_Print_EditPrimary_Desktop-Small.jpg?auto=webp&format=pjpg&quality=75&dpr=2&width=1246"
+                            alt="slider-1" className='w-full h-full object-cover rounded-lg'/>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src="https://images-cs.stockx.com/v3/assets/blt818b0c67cf450811/bltade94cf09eea34b2/67b7ebc75dcb773a66dcf97d/EN-Nike_Foamposite_Galaxy_(New_Release)Primary_Desktop-Small.jpg?auto=webp&format=pjpg&quality=75&dpr=2&width=1246"
+                            alt="slider-1" className='w-full h-full object-cover rounded-lg'/>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src="https://images-cs.stockx.com/v3/assets/blt818b0c67cf450811/bltade94cf09eea34b2/67b7ebc75dcb773a66dcf97d/EN-Nike_Foamposite_Galaxy_(New_Release)Primary_Desktop-Small.jpg?auto=webp&format=pjpg&quality=75&dpr=2&width=1246"
+                            alt="slider-1" className='w-full h-full object-cover rounded-lg'/>
+                    </SwiperSlide>
+            </Swiper>
+            </div>
+        </div>
+    )
+}
+
+export default HomeSlider;
---
+++ frontend/src/components/HomeCatSlider/index.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+// Import Navigation styles
+import 'swiper/css/navigation';
+
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+// import required modules
+import { Navigation } from 'swiper/modules';
+import { Pagination } from 'swiper/modules';
+import { Autoplay } from 'swiper/modules';
+
+const HomeCatSlider = () => {
+    return (
+        <div className='homeCatSlider'>
+            <div className='container rounded-[10px]'>
+            <Swiper navigation={true} spaceBetween={10} slidesPerView={5} loop={true}
+                pagination={{
+                clickable: true,
+                }}
+                centeredSlides={true}
+                autoplay={{
+                    delay: 1000,
+                    disableOnInteraction: false,
+                }}
+                modules={[Autoplay, Navigation, Pagination]} className="mySwiper">
+                        <SwiperSlide>
+                            <img src="https://images-cs.stockx.com/v3/assets/blt818b0c67cf450811/blte08f252776d5834f/6773260836f5cc36f9db1b35/Copy_of_Brand_Tile_Template_(10).png?auto=webp&format=pjpg&width=234&height=160&dpr=2&quality=75"
+                                alt="slider-1" className='w-full h-full object-cover rounded-lg'/>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img src="https://images-cs.stockx.com/v3/assets/blt818b0c67cf450811/bltc5a55f409dcd724d/6773257bb290b35ed991f4f6/Copy_of_Brand_Tile_Template_(9).png?auto=webp&format=pjpg&width=234&height=160&dpr=2&quality=75"
+                                alt="slider-1" className='w-full h-full object-cover rounded-lg'/>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img src="https://images-cs.stockx.com/v3/assets/blt818b0c67cf450811/blt7de4e73e15b4ad06/67a2992aecc9d7c5f9413fce/Copy_of_Brand_Tile_Template_(24).png?auto=webp&format=pjpg&width=234&height=160&dpr=2&quality=75"
+                                alt="slider-1" className='w-full h-full object-cover rounded-lg'/>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img src="https://images-cs.stockx.com/v3/assets/blt818b0c67cf450811/blt53990f215de13e8e/67732787775da7112d1f51d0/Copy_of_Brand_Tile_Template_(13).png?auto=webp&format=pjpg&width=234&height=160&dpr=2&quality=75"
+                                alt="slider-1" className='w-full h-full object-cover rounded-lg'/>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img src="https://images-cs.stockx.com/v3/assets/blt818b0c67cf450811/blt67260f9960b40bc3/67732801776bc04ff327d1fc/Copy_of_Brand_Tile_Template_(12).png?auto=webp&format=pjpg&width=234&height=160&dpr=2&quality=75"
+                                alt="slider-1" className='w-full h-full object-cover rounded-lg'/>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img src="https://images-cs.stockx.com/v3/assets/blt818b0c67cf450811/bltaa06881e37221628/67732f30258d13663490765a/Copy_of_Brand_Tile_Template_(19).png?auto=webp&format=pjpg&width=234&height=160&dpr=2&quality=75"
+                                alt="slider-1" className='w-full h-full object-cover rounded-lg'/>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img src="https://images-cs.stockx.com/v3/assets/blt818b0c67cf450811/blt39ab15307f05ef1f/67732f473881b884dfa6fceb/Copy_of_Brand_Tile_Template_(20).png?auto=webp&format=pjpg&width=234&height=160&dpr=2&quality=75"
+                                alt="slider-1" className='w-full h-full object-cover rounded-lg'/>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img src="https://images-cs.stockx.com/v3/assets/blt818b0c67cf450811/blt27f501d78dcb3f1a/67732f5b5754575b2d18f8b0/Copy_of_Brand_Tile_Template_(21).png?auto=webp&format=pjpg&width=234&height=160&dpr=2&quality=75"
+                                alt="slider-1" className='w-full h-full object-cover rounded-lg'/>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img src="https://images-cs.stockx.com/v3/assets/blt818b0c67cf450811/blt7429867edc048844/67732f6f91eb612d092ffe92/Copy_of_Brand_Tile_Template_(22).png?auto=webp&format=pjpg&width=234&height=160&dpr=2&quality=75"
+                                alt="slider-1" className='w-full h-full object-cover rounded-lg'/>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img src="https://images-cs.stockx.com/v3/assets/blt818b0c67cf450811/blt80bee13092b482f2/67732fa53c2538f95a759403/Copy_of_Brand_Tile_Template_(23).png?auto=webp&format=pjpg&width=234&height=160&dpr=2&quality=75"
+                                alt="slider-1" className='w-full h-full object-cover rounded-lg'/>
+                        </SwiperSlide>
+                </Swiper>
+            </div>
+        </div>
+    )
+}
+
+export default HomeCatSlider;
